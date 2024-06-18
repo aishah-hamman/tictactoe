8d9c90d42a2785ccc5c8422d50c9390921423bfd
Aishah Hamman <aishahhamman2016@gmail.com>
2024-06-18 23:48:07 +0300

    first commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,28 @@
-import React from "react";
+import { useState } from "react";
 
-export default function App() {
-	return <h1 className="text-red-500">App</h1>;
+function Square({ value }) {
+	return <button className="square">{value}</button>;
+}
+
+export default function Board() {
+	const [squares, setSquares] = useState(Array(9).fill(null));
+	return (
+		<>
+			<div className="board-row">
+				<Square value={squares[0]} />
+				<Square value={squares[1]} />
+				<Square value={squares[2]} />
+			</div>
+			<div className="board-row">
+				<Square value={squares[3]} />
+				<Square value={squares[4]} />
+				<Square value={squares[5]} />
+			</div>
+			<div className="board-row">
+				<Square value={squares[6]} />
+				<Square value={squares[7]} />
+				<Square value={squares[8]} />
+			</div>
+		</>
+	);
 }
